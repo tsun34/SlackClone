1fad60450e01951ccc7d711e46f8b48d58c918fe
CharSun <tingxiao.sun@gmail.com>
2020-08-24 14:02:03 -0700

    update chatroom

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,9 @@ class User < ApplicationRecord
     has_many :subscriptions,
         foreign_key: :user_id,
         class_name: :Subscription
+    
+    has_many :conversations,
+        through: :subscriptions
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
