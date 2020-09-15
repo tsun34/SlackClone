197ab9c5c10d3985ab0e5daf79a6ab6fcefd3920
CharSun <tingxiao.sun@gmail.com>
2020-09-15 11:07:53 -0700

    push to heroku

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ApplicationRecord
+    validates :user_id, :conversation_id, presence: true
+    
     belongs_to :user,
         foreign_key: :user_id,
         class_name: :User 
